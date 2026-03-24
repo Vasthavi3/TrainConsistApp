6408a730ec808c5dc2725b89fb2479afc9387d00
Vasthavi3 <vd9840@srmist.edu.in>
2026-03-24 14:34:45 +0530

    UC1: Initialize train and display consist summary

---
+++ TrainConsistApp.java
@@ -0,0 +1,11 @@
+public class TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        String trainName = "Express Train";
+        int totalBogies = 5;
+
+        System.out.println("🚆 Train Name: " + trainName);
+        System.out.println("Total Bogies: " + totalBogies);
+    }
+}
